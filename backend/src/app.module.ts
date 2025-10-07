2eae45b738a5ddd818bcfa5cc7af090539e7031d
Ignacio Cenni <ignaziocenni@gmail.com>
2025-10-06 22:20:56 -0300

    feat(backend): implement initial application structure with controllers, services, and testing

---
+++ backend/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    // Configuración para leer el archivo .env
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    
+    // Conexión a MongoDB Atlas (con validación)
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
